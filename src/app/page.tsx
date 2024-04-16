35a06b1b6f2ca98529c5e09f6a102655b79f03eb
Ryan Barley <rmbarley@gmail.com>
2024-04-15 22:58:01 -0400

    Add db and read from it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-const mockUrls = [
-  "https://utfs.io/f/ad5c4153-c5b1-4701-8d84-7a3ee3191deb-tccc7s.jpg",
-  "https://utfs.io/f/f4e7a8fa-6a5e-41a9-837f-9535d8c95172-r64k6c.jpg",
-  "https://utfs.io/f/d9c6ed82-6a3d-4e2f-b04b-2950fc12af10-2f98cg.jpg",
-  "https://utfs.io/f/51a33f19-2738-4309-a0b0-2ec2254d81fb-p9lavc.jpg",
-];
+export const dynamic = "force-dynamic";
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
-export default function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48 p-4">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.name + "-" + index} className="flex w-48 flex-col">
             <img src={image.url} />
+            <p>{image.name}</p>
           </div>
         ))}
       </div>
